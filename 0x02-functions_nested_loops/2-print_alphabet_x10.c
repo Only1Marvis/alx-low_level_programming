664111e51c56d920675e01d6c03166e8bc852ef3
Only1Marvis <marvisenubiaka@gmail.com>
2022-07-06 17:22:54 -0700

    Mandatory task commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,13 @@
-#include "main.h"
-
-
-/**
+/*
  *File: 2-print_alphabet_x10.c
  *Auth: Marvis Onyenwenu Enubiaka
  */
 
+#include "main.h"
+
 /**
- *main - Prints 10 times the alphabet, in lowercase.
- *Return: Always 0 (Success)
+ *print_alphabet_x10 - Prints 10 times the alphabet, in lowercase. 
  */
-
-
 void print_alphabet_x10(void)
 {
 	int count = 0;
@@ -21,6 +17,6 @@ void print_alphabet_x10(void)
 	{
 	for (letter = 'a'; letter <= 'z'; letter++)
 	_putchar(letter);
-	_putchar('\n');
+	_putchar('/n');
 	}
 }
